321dfed4b4878823878bf413c381805d14c10aa9
ZARIFMAC <uzbzarif@gmail.com>
2022-10-06 16:31:01 +0500

    home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'page1.dart';
+import 'page2.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,16 +15,32 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(title: Text('Home page')),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Next Page'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: ((context) {
-                return Page1();
-              })),
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Page 1'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: ((context) {
+                    return Page1();
+                  })),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Page 2'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: ((context) {
+                    return Page2();
+                  })),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
