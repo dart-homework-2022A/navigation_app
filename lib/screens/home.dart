ac26855f31eb450ae653f4ee096b208d9d0f184b
ZARIFMAC <uzbzarif@gmail.com>
2022-10-06 16:13:10 +0500

    nextpage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,6 +12,28 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Next Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: ((context) {
+                return NextPage();
+              })),
+            );
+          },
+        ),
+      ),
     );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
