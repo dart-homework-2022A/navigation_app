45b3c48da58e2740f3cd6547b1dc96dcdcce59d7
ZARIFMAC <uzbzarif@gmail.com>
2022-10-08 16:15:44 +0500

    router

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/page1.dart';
 import 'screens/page2.dart';
+import 'router/router.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'home',
     routes: {
-      'home': (context) => Home(),
-      'page1': (context) => Page1(),
-      'page2': (context) => Page2(),
+      homePage: (context) => Home(),
+      page1: (context) => Page1(),
+      page2: (context) => Page2(),
     },
   ));
 }
